0e832a40bffa641b24880d3ac2e21cbe34c7977d
Daniel Brodsky <danbrodsky@pm.me>
2021-07-04 23:32:33 -0700

    updated project build and dependency system

--- setup.py
+++ setup.py
@@ -7,24 +7,6 @@ import sys
 from glob import glob
 from setuptools import setup
 
-import DistUtilsExtra.command.build_extra
-import DistUtilsExtra.command.build_i18n
-import DistUtilsExtra.command.clean_i18n
-
-# to update i18n .mo files (and merge .pot file into .po files) run on Linux:
-#   tx pull -a --minimum-perc=5
-#   python3 setup.py build_i18n -m
-#   tx push -s
-
-# silence pyflakes, __VERSION__ is properly assigned below...
-__VERSION__ = '0.0.0.0'
-with open('mirrus') as f:
-    for line in f:
-        if (line.startswith('__VERSION__')):
-            exec(line.strip())
-            break
-
-PROGRAM_VERSION = __VERSION__
 
 def datafilelist(installbase, sourcebase):
     datafileList = []
@@ -35,29 +17,39 @@ def datafilelist(installbase, sourcebase):
         datafileList.append((root.replace(sourcebase, installbase), fileList))
     return datafileList
 
-data_files = [
-    ('{prefix}/share/man/man1'.format(prefix=sys.prefix), glob('data/*.1')),
-    ('{prefix}/share/applications'.format(prefix=sys.prefix), ['data/mirrus.desktop',]),
-    ('/etc/xdg/autostart'.format(prefix=sys.prefix), ['data/mirrus-autostart.desktop',]),
-]
-#data_files.extend(datafilelist('{prefix}/share/locale'.format(prefix=sys.prefix), 'build/mo'))
 
-cmdclass ={
-            "build" : DistUtilsExtra.command.build_extra.build_extra,
-            "build_i18n" :  DistUtilsExtra.command.build_i18n.build_i18n,
-            "clean": DistUtilsExtra.command.clean_i18n.clean_i18n,
-}
+data_files = [
+    ("{prefix}/share/man/man1".format(prefix=sys.prefix), glob("data/*.1")),
+    (
+        "{prefix}/share/applications".format(prefix=sys.prefix),
+        [
+            "data/mirrus.desktop",
+        ],
+    ),
+    (
+        "{prefix}/etc/xdg/autostart".format(prefix=sys.prefix),
+        [
+            "data/mirrus-autostart.desktop",
+        ],
+    ),
+]
+
+version = (
+    next(l for l in open("pyproject.toml").read().splitlines() if "version = " in l)
+    .split('version = "')[1]
+    .split('"')[0]
+)
 
 setup(
-    name = "Mirrus",
-    version = PROGRAM_VERSION,
-    description = "Screen-sharing for large displays",
-    license = 'MIT',
-    author = 'Daniel Brodsky',
-    url = 'https://github.com/danbrodsky/mirrus',
-    package_dir = {'': '.'},
-    data_files = data_files,
-    install_requires = [ 'setuptools', ],
-    scripts = ['mirrus'],
-    cmdclass = cmdclass,
+    name="Mirrus",
+    version=version,
+    description="Screen-sharing for large displays",
+    license="MIT",
+    author="Daniel Brodsky",
+    data_files=data_files,
+    install_requires=[
+        "setuptools",
+    ],
+    python_requires=">=3.6.*",
+    scripts=["mirrus/__init__.py"],
 )
